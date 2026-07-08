27793d3b98311667f6e37a7ea80a407f6c1bb7c4
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-07-08 22:40:31 +0700

    feat: query soal 1 skema 1

--- Latihan2/Query/Skema-1/Soal-1.sql
+++ Latihan2/Query/Skema-1/Soal-1.sql
@@ -1,2 +1,19 @@
 SET search_path TO perpustakaan, public;
-select * from buku;
+
+with jumlah_peminjaman as (
+select e.buku_id, count(p.peminjaman_id) as total_borrowings 
+from peminjaman p 
+join eksemplar e on p.eksemplar_id = e.eksemplar_id 
+where p.tanggal_pinjam >= current_date - interval '12 months' 
+group by e.buku_id),
+
+ranking_peminjaman as (
+select buku_id, total_borrowings, 
+dense_rank() over(order by total_borrowings desc) as borrowing_rank 
+from jumlah_peminjaman)
+
+select b.judul as title, rp.total_borrowings, rp.borrowing_rank 
+from ranking_peminjaman rp 
+join buku b on b.buku_id = rp.buku_id 
+where borrowing_rank <= 5 
+order by rp.borrowing_rank, b.judul;
